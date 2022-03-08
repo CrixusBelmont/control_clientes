1dd7ebc0e087782594f826bdf3d6613dd8f3def4
El exótico Christian <christian.gonzalezdegracia@gmail.com>
2022-03-07 19:47:37 -0500

    first commit

---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,11 @@
+export const environment = {
+  production: true,
+  firestore:{
+    apiKey: "AIzaSyAY44WL4PNCI5BKkLN68BQxLGt4AikCArk",
+    authDomain: "control-clientes-a10c6.firebaseapp.com",
+    projectId: "control-clientes-a10c6",
+    storageBucket: "control-clientes-a10c6.appspot.com",
+    messagingSenderId: "234529966273",
+    appId: "1:234529966273:web:26ab7de460b1fc147dec40",
+  }
+};
